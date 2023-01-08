543464e2f305eadaec2f0c7418330c53e6acb43f
Kevin Liang <47095084+unko-chan@users.noreply.github.com>
2023-01-08 00:44:25 -0800

    Update test.js

--- test.js
+++ test.js
@@ -8,19 +8,20 @@ let currTerrain = path[segment];
 // set constant
 let fuelRate = 202
 let fuelAvg0 = 54.88
+let fuelAvg1 = 61.53
 let fuelAvgTotal = 111.19
-let fuelMultiplier = 
-
+let fuelMultiplier = 5 // change value to match irl-like value
+let scoreMultiplier = 7 // change value depending on how big we want scores to be
 
 
 // to display fuelConsumed throughout gameplay
 function fuel() {
     if (currTerrain === 'FLAT') {
-        fuelConsumed += timeElapsed * fuelRate
+        fuelConsumed += timeElapsed * fuelRate * fuelMultiplier 
     } else if (currTerrain === 'DECLINE') {
-        fuelConsumed += timeElapsed * fuelRate * 0.8
+        fuelConsumed += timeElapsed * fuelRate * fuelMultiplier * 0.8
     } else if (currTerrain === 'INCLINE') {
-        fuelConsumed += timeElapsed * fuelRate * 1.4
+        fuelConsumed += timeElapsed * fuelRate * fuelMultiplier * 1.4
     }
 }
 
@@ -29,9 +30,9 @@ function fuel() {
 // call this function @ end of gameLeg0 & gameLeg1
 function score() {
     if (gameLeg === 0) {
-        score = fuelConsumed / fuelAvg0
+        score = (fuelAvg0 - fuelConsumed) * 2.2
     } else if (gameLeg === 1) {
-        score = fuelConsumed / fuelAvgTotal
+        score = (fuelConsumed / fuelAvg1) * scoreMultiplier
     }
 }
 
@@ -39,21 +40,21 @@ function score() {
 // time elapsed based on when player clicks Start and End buttons
 var startTime, endTime;
 
-    function start() {
-        startTime = new Date();
-    }
+function start() {
+    startTime = new Date();
+}
 
-    function end() {
-        endTime = new Date();
-        var timeDiff = endTime - startTime; // this is in milliseconds
-        timeDiff /= 1000; // strip the ms???
+function end() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; // this is in milliseconds
+    timeDiff /= 1000; // strip the ms
 
-        // convert to seconds
-        var seconds = Math.round(timeDiff);
-        console.log(seconds + " seconds");
-        
-        // document.getelementByID("asdf").innerHTML = seconds 
-    }
+    // convert to seconds
+    var seconds = Math.round(timeDiff);
+    console.log(seconds + " seconds");
+    
+    // document.getelementByID("asdf").innerHTML = seconds 
+}
 
 
 
