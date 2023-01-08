ed70268956e6fa3e86f95f92e009a8164d9b83ce
Kevin Liang <47095084+unko-chan@users.noreply.github.com>
2023-01-07 21:43:57 -0800

    a

--- Game/script.js
+++ Game/script.js
@@ -4,24 +4,54 @@ window.addEventListener("load", function () {
   canvas.width = 800;
   canvas.height = 720;
 
+  const DECLINE = {
+    terrain: "decline",
+    pathX: 300,
+    pathY: 250,
+    accelerationRate: 0.2,
+    decelerationRate: -0.01,
+    maxSpeed: 4,
+  };
+
+  const FLAT = {
+    terrain: "flat",
+    pathX: 300,
+    pathY: 0,
+    accelerationRate: 0.05,
+    decelerationRate: -0.05,
+    maxSpeed: 3,
+  };
+
+  const INCLINE = {
+    terrain: "incline",
+    pathX: 300,
+    pathY: -250,
+    accelerationRate: 0.01,
+    decelerationRate: -0.2,
+    maxSpeed: 2,
+  };
+
   const path = [
-    { type: "flat", pathX: 300, pathY: 0 },
-    { type: "decline", pathX: 300, pathY: 250 },
-    { type: "flat", pathX: 300, pathY: 0 },
-    { type: "decline", pathX: 300, pathY: 250 },
-    { type: "flat", pathX: 300, pathY: 0 },
-    { type: "flat", pathX: 300, pathY: 0 },
-    { type: "incline", pathX: 300, pathY: -250 },
-    { type: "flat", pathX: 300, pathY: 0 },
-    { type: "incline", pathX: 300, pathY: -250 },
-    { type: "flat", pathX: 3000, pathY: 0 },
+    FLAT,
+    DECLINE,
+    FLAT,
+    DECLINE,
+    FLAT,
+    FLAT,
+    INCLINE,
+    FLAT,
+    INCLINE,
+    FLAT,
   ];
 
-  let globalDistance = -1; //groundX
-  let globalAcceleration = 0;
-  let globalGround = 100;
-  let globalGrid = 0;
+  let distanceTravelled = 0; //groundX
+  // console.log("segment", segment)
+  let gameLeg = Math.floor(distanceTravelled / 1500);
   let playerY = 100;
+  let segment = Math.floor(distanceTravelled / 300);
+  let currTerrain = path[segment];
+  let timer = 0
+  console.log("segment", segment);
 
   class InputHandler {
     constructor() {
@@ -54,26 +84,11 @@ window.addEventListener("load", function () {
       this.speed = 0;
     }
     draw(context) {
+      if (segment === 1) {
+        playerY = (250 / 300) * (Math.abs(this.x)) + 100;
+      }
+      console.log(this.y)
       this.y = playerY;
-      // //flats
-      // if (
-      //   grid === 0 ||
-      //   grid === 2 ||
-      //   grid === 4 ||
-      //   grid === 5 ||
-      //   grid === 7 ||
-      //   grid === 9
-      // ) {
-      // }
-
-      // //decline
-      // if (grid === 1 || grid === 3) {
-      // }
-
-      // //incline
-      // if (grid === 6 || grid === 8) {
-      // }
-
       context.beginPath();
       context.setLineDash([]);
       context.arc(this.x, this.y, 5, 0, 2 * Math.PI);
@@ -83,16 +98,37 @@ window.addEventListener("load", function () {
   }
 
   class Ground {
-    constructor(gameWidth, gameHeight, globalDistance) {
-      this.gameWidth = 1000;
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
       this.gameHeight = gameHeight;
-      this.x = globalDistance;
+      this.x = 0;
       this.y = 100;
       this.speed = 0;
+      // update max_speed based to make it more realistic (max speed of the truck in data was 60km/h for a 6km course)
+      // this.max_speed = 3;
+    }
+    acceleration(input) {
+      let map = {
+        accel: currTerrain.accelerationRate,
+        decel: currTerrain.decelerationRate,
+        maxSpeed: currTerrain.maxSpeed,
+      };
+      // console.log(currTerrain.terrain)
+
+      // if you are able to accelerate
+      if (input === true) {
+        // console.log("true");
+        // determine the acceleration factor using the terrain
+        this.speed = Math.min(this.speed + map.accel, map.maxSpeed);
+      } else if (input === false) {
+        this.speed = Math.max(0, this.speed + map.decel);
+        // this.speed = 0
+        // console.log(this.speed);
+      }
     }
 
     draw(context) {
-      this.x = globalDistance;
+      // this.x = distanceTravelled;
       context.beginPath();
       context.moveTo(0, this.y);
 
@@ -117,61 +153,80 @@ window.addEventListener("load", function () {
 
     update(input) {
       this.x -= this.speed;
+      distanceTravelled += this.speed;
+      segment = Math.floor((distanceTravelled + 100) / 300);
+      console.log(segment);
+      currTerrain = path[segment];
       if (input.keys.indexOf("ArrowRight") > -1) {
-        this.speed = 1;
+        this.acceleration(true);
+        // this.speed = 2;
       } else {
-        this.speed = 0;
+        this.acceleration(false);
+        // this.speed = 0;
       }
+      // console.log(this.y);
     }
   }
 
-  function drive(input) {
-    let playerX = globalDistance - 100;
-    // console.log(playerX);
+  // function drive(input) {
+  //   let playerX = distanceTravelled - 100;
+  //   // console.log(playerX);
 
-    globalDistance -= globalAcceleration;
-    if (input.keys.indexOf("ArrowRight") > -1) {
-      globalAcceleration = 1;
-    } else {
-      globalAcceleration = 0;
-    }
-    if (Math.abs(playerX) % 300 === 0) {
-      globalGrid++;
-    }
-    //flats
-    if (
-      globalGrid === 0 ||
-      globalGrid === 2 ||
-      globalGrid === 4 ||
-      globalGrid === 5 ||
-      globalGrid === 7 ||
-      globalGrid === 9
-    ) {
-    }
+  //   distanceTravelled -= globalAcceleration;
+  //   if (input.keys.indexOf("ArrowRight") > -1) {
+  //     globalAcceleration = 1;
+  //   } else {
+  //     globalAcceleration = 0;
+  //   }
+  //   if (Math.abs(playerX) % 300 === 0) {
+  //     globalGrid++;
+  //   }
+  //   //flats
+  //   if (
+  //     globalGrid === 0 ||
+  //     globalGrid === 2 ||
+  //     globalGrid === 4 ||
+  //     globalGrid === 5 ||
+  //     globalGrid === 7 ||
+  //     globalGrid === 9
+  //   ) {
+  //   }
 
-    //decline
-    if (globalGrid === 1 || globalGrid === 3) {
-      console.log((Math.abs(playerX) - 300))
-      playerY = (250 / 300) * (Math.abs(playerX) - 300);
-    }
-    //incline
-    if (globalGrid === 6 || globalGrid === 8) {
-    }
-    // console.log(playerY)
-  }
+  //   //decline
+  //   if (globalGrid === 1) {
+  //     console.log((Math.abs(playerX) - 300))
+  //     playerY = (250 / 300) * (Math.abs(playerX) - 300) + 100;
+  //   }
+
+  //   if (globalGrid === 3){
+  //     playerY = (250 / 300) * (Math.abs(playerX) - 600) + 100;
+  //   }
+  //   //incline
+  //   if (globalGrid === 6) {
+  //     playerY = Math.abs(-250 / 300) * (Math.abs(playerX)) + 100;
+  //   }
+
+  //   if(globalGrid === 8) {
+  //     playerY = (-250 / 300) * (Math.abs(playerX) + 0);
+
+  //   }
+  //   console.log(playerY)
+  // }
   class Background {}
 
   function displayStatusText() {}
 
   const input = new InputHandler();
-  const ground = new Ground(canvas.width, canvas.height, globalDistance);
+  const ground = new Ground(canvas.width, canvas.height, distanceTravelled);
   const player = new Player(canvas.width, canvas.height, ground.y, ground.x);
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.draw(ctx);
     ground.draw(ctx);
-    // ground.update(input);
-    drive(input);
+    ground.update(input);
+    timer += 0.016666
+    console.log(timer)
+    // drive(input);
     requestAnimationFrame(animate);
   }
   animate();
---
+++ test.js
@@ -0,0 +1,59 @@
+// globals
+let fuelConsumed = 0
+let timeElapsed = 0
+
+// already set global
+let currTerrain = path[segment];
+
+// set constant
+let fuelRate = 202
+let fuelAvg0 = 54.88
+let fuelAvgTotal = 111.19
+let fuelMultiplier = 
+
+
+
+// to display fuelConsumed throughout gameplay
+function fuel() {
+    if (currTerrain === 'FLAT') {
+        fuelConsumed += timeElapsed * fuelRate
+    } else if (currTerrain === 'DECLINE') {
+        fuelConsumed += timeElapsed * fuelRate * 0.8
+    } else if (currTerrain === 'INCLINE') {
+        fuelConsumed += timeElapsed * fuelRate * 1.4
+    }
+}
+
+
+// to display score (cost saved) at @ end of gameLeg0 & gameLeg1
+// call this function @ end of gameLeg0 & gameLeg1
+function score() {
+    if (gameLeg === 0) {
+        score = fuelConsumed / fuelAvg0
+    } else if (gameLeg === 1) {
+        score = fuelConsumed / fuelAvgTotal
+    }
+}
+
+
+// time elapsed based on when player clicks Start and End buttons
+var startTime, endTime;
+
+    function start() {
+        startTime = new Date();
+    }
+
+    function end() {
+        endTime = new Date();
+        var timeDiff = endTime - startTime; // this is in milliseconds
+        timeDiff /= 1000; // strip the ms???
+
+        // convert to seconds
+        var seconds = Math.round(timeDiff);
+        console.log(seconds + " seconds");
+        
+        // document.getelementByID("asdf").innerHTML = seconds 
+    }
+
+
+
